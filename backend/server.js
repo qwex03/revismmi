c5af78dd3cdebcbb53049053dd9e6902355df63b
qwex03 <qlecordier@gmail.com>
2025-01-15 10:23:54 +0100

    ajout de l'id user lors de l'envoi d'un fichier

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,15 @@ app.post('/upload', upload.single('file'), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: 'Aucun fichier téléchargé' });
   }
+  const { userId } = req.body;
   res.status(200).json({
     message: 'Fichier téléchargé avec succès',
     file: {
       originalname: req.file.originalname,
       filename: req.file.filename,
       path: `/uploads/${req.file.filename}` // Changed from 'uploads' to 'docs'
-    }
+    },
+    userId: userId,
   });
 
   // Execute main.mjs after upload
